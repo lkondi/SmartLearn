60bb567a4dc9add7bd8353179ad29bcd01352fca
Wanja <w.sajko@gmail.com>
2018-12-29 16:08:29 +0100

    added attributes to User. added two constructors to User:  1. basic constructor  2. User will be created based on a JSONObject added a method "convertToJASON" which converts this User into a JSONObject

--- app/src/main/java/com/example/lkondilidis/smartlearn/model/User.java
+++ app/src/main/java/com/example/lkondilidis/smartlearn/model/User.java
@@ -1,6 +1,7 @@
 package com.example.lkondilidis.smartlearn.model;
 
-import android.graphics.Bitmap;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class User {
 
@@ -8,7 +9,29 @@ public class User {
     private String name;
     private String email;
     private String password;
+    private double rating;
+    private String privateInfo;
 
+    public User(int id, String name, String email, String password, double rating, String privateInformation){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.rating = rating;
+        this.privateInfo = privateInformation;
+    }
+
+    public User(JSONObject jsonObject){
+        try {
+            this.id = jsonObject.getInt("id");
+            this.name = jsonObject.getString("name");
+            this.email = jsonObject.getString("email");
+            this.rating = jsonObject.getDouble("rating");
+            this.privateInfo = jsonObject.getString("privateInfo");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
     public int getId() {
         return id;
@@ -40,6 +63,36 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getPrivateInfo() {
+        return privateInfo;
+    }
+
+    public void setPrivateInfo(String privateInfo) {
+        this.privateInfo = privateInfo;
+    }
+
+    public JSONObject convertToJASON(){
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("id", getId());
+            jsonObject.put("name", getName());
+            jsonObject.put("email", getEmail());
+            jsonObject.put("rating", getRating());
+            jsonObject.put("privateInfo", getPrivateInfo());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return  jsonObject;
     }
 
 }
