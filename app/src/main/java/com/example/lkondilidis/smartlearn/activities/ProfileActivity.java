1faad1c0d9791ab5d937033ddb308e5628a68e17
lkondilidis <lidia.kondilidis@yahoo.com>
2018-12-31 01:08:14 +0100

    user profile updates

--- app/src/main/java/com/example/lkondilidis/smartlearn/activities/ProfileActivity.java
+++ app/src/main/java/com/example/lkondilidis/smartlearn/activities/ProfileActivity.java
@@ -1,22 +1,40 @@
 package com.example.lkondilidis.smartlearn.activities;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatTextView;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View;
 import com.example.lkondilidis.smartlearn.R;
+import com.example.lkondilidis.smartlearn.helpers.InputValidation;
 import com.example.lkondilidis.smartlearn.sql.DatabaseHelper;
 import com.example.lkondilidis.smartlearn.model.User;
 
 import java.util.ArrayList;
 
 public class ProfileActivity extends AppCompatActivity {
+    private final AppCompatActivity activity = ProfileActivity.this;
 
     TextView textViewName,textViewEmail,textViewNickname, textViewStudies, textViewSubject, textViewPlan, textViewBewertungen;
     DatabaseHelper dataBaseHelper;
+
+    private AppCompatButton appCompatButtonSubmit;
+
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutPassword;
+
+    private TextInputEditText textInputEditTextEmail;
+    private TextInputEditText textInputEditTextPassword;
+
+    private static String STRING_EMPTY = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,25 +42,68 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         dataBaseHelper = new DatabaseHelper(this);
 
-        initView();
-    }
-    public void initView(){
-
-        Intent intent = getIntent();
-        String email = intent.getStringExtra("EMAIL");
 
         textViewName= (TextView) findViewById(R.id.name);
         textViewEmail = (TextView) findViewById(R.id.email);
-        textViewNickname = (TextView) findViewById(R.id.nickname);
-        textViewStudies = (TextView) findViewById(R.id.studies1);
-        textViewSubject = (TextView) findViewById(R.id.subject1);
-        textViewPlan = (TextView) findViewById(R.id.plan1);
-        textViewBewertungen = (TextView) findViewById(R.id.bewertungen1);
 
-        textViewEmail.setText(email);
+        textInputLayoutEmail = (TextInputLayout) findViewById(R.id.textInputLayoutEmail);
+        textInputLayoutPassword = (TextInputLayout) findViewById(R.id.textInputLayoutPassword);
 
-        textViewName.setText(dataBaseHelper.getUser(email).getName());
+        textInputEditTextEmail = (TextInputEditText) findViewById(R.id.textInputEditTextEmail);
+        textInputEditTextPassword = (TextInputEditText) findViewById(R.id.textInputEditTextPassword);
 
+
+        String emailFromIntent = getIntent().getStringExtra("EMAIL");
+        dataBaseHelper = new DatabaseHelper(activity);
+        textViewEmail.setText(emailFromIntent);
+        textViewName.setText(dataBaseHelper.getUser(emailFromIntent).getName());
+
+        appCompatButtonSubmit = (AppCompatButton) findViewById(R.id.appCompatButtonSubmit);
+        appCompatButtonSubmit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                updateUser();
+            }
+        });
+    }
+
+
+
+    private void updateUser() {
+
+        String emailFromIntent = getIntent().getStringExtra("EMAIL");
+
+            if (!STRING_EMPTY.equals(textInputEditTextEmail.getText().toString()) &&
+                    !STRING_EMPTY.equals(textInputEditTextPassword.getText().toString())) {
+
+                User userDetails = dataBaseHelper.getUser(emailFromIntent);
+
+                userDetails.setEmail(textInputEditTextEmail.getText().toString());
+                userDetails.setPassword(textInputEditTextPassword.getText().toString());
+
+                dataBaseHelper.updateUser(userDetails);
+
+                emptyInputEditText();
+
+                textViewEmail.setText(userDetails.getEmail());
+
+                Toast.makeText(ProfileActivity.this, "EDITED!",
+                        Toast.LENGTH_LONG).show();
+
+
+            } else {
+                Toast.makeText(ProfileActivity.this, "One or more fields left empty!",
+                        Toast.LENGTH_LONG).show();
+            }
 
     }
+
+    /**
+     * empty all input edit text
+     */
+    private void emptyInputEditText() {
+        textInputEditTextEmail.setText(null);
+        textInputEditTextPassword.setText(null);
+    }
+
+
 }
