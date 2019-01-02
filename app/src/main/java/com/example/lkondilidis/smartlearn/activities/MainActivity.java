912348ba8c9a19640014f436feca73145e4c785c
Wanja <w.sajko@gmail.com>
2019-01-02 20:04:11 +0100

    Merge branch 'master' of https://github.com/lkondi/SmartLearn
    # Conflicts:
    #	app/src/main/java/com/example/lkondilidis/smartlearn/sql/SQLiteDBHelper.java
    
    change the userListView to a recyclerView

--- app/src/main/java/com/example/lkondilidis/smartlearn/activities/MainActivity.java
+++ app/src/main/java/com/example/lkondilidis/smartlearn/activities/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -21,11 +22,14 @@ import com.example.lkondilidis.smartlearn.R;
 import com.example.lkondilidis.smartlearn.adapters.SearchAdapter;
 import com.example.lkondilidis.smartlearn.model.User;
 import com.example.lkondilidis.smartlearn.services.*;
+import com.example.lkondilidis.smartlearn.sql.SQLiteDBHelper;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, SearchView.OnSuggestionListener {
 
+    private final AppCompatActivity activity = MainActivity.this;
+    public static final String USER_DETAIL_KEY = "selecteduser";
     ActionBarDrawerToggle mToggle;
     DrawerLayout drawerLayout;
     SearchView searchView;
@@ -33,8 +37,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     RecyclerView.LayoutManager layoutManager;
     SearchAdapter searchAdapter;
 
+    ArrayList<User> userArrayList;
+    SQLiteDBHelper databaseHelper;
+
     ArrayAdapter<String> arrayAdapter;
-    User[] users = {new User(1, "Wanja", null, null, 0, null), new User(1, "Lydia", null, null, 0, null)};
     String[] lectures = {"Analysis", "MSP", "Datenbanksysteme"};
 
     @Override
@@ -42,14 +48,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Drawer
         drawerLayout = findViewById(R.id.drawer);
         NavigationView navigationView = findViewById(R.id.navigation_view);
         mToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
-
         navigationView.setNavigationItemSelectedListener(this);
         drawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
 
+        //Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionbar = getSupportActionBar();
@@ -58,9 +65,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
         //Test
-        ArrayList<User> userArrayList = new ArrayList<>();
-        userArrayList.add(new User(1, "Wanja", "example@lmu.de", null, 0, "sadsasfasgasasas"));
-        userArrayList.add(new User(2, "Lydia", "example2@lmu.de", null, 0, "!!!!!!!!!!!!!"));
+        userArrayList = new ArrayList<>();
+        //userArrayList.add(new User(1, "Wanja", "example@lmu.de", null, 0, "sadsasfasgasasas"));
+        //userArrayList.add(new User(2, "Lydia", "example2@lmu.de", null, 0, "!!!!!!!!!!!!!"));
         //TODO: create Database for useres
 
         //!!!!!!
@@ -70,24 +77,30 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
 
-        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lectures);
+        //arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lectures);
 
 
+        //recyclerView
         recyclerView = (RecyclerView) findViewById(R.id.recycler_search);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
         searchAdapter = new SearchAdapter(this, userArrayList);
         recyclerView.setAdapter(searchAdapter);
+        fetchUsers();
 
 
 
-        //ListView myListView = findViewById(R.id.myListView);
-        //ItemAdapter ia = new ItemAdapter(this, userArrayList);
-        //myListView.setAdapter(ia);
 
         searchView = findViewById(R.id.searchView);
-
+        searchView.setOnSuggestionListener(this);
+        searchView.setSuggestionsAdapter(new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1,
+                null,
+                lectures,
+                new int[]{android.R.layout.simple_list_item_1},
+                0));
+        searchView.setSubmitButtonEnabled(true);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
@@ -96,48 +109,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
             @Override
             public boolean onQueryTextChange(String s) {
-                //arrayAdapter.getFilter().filter(s);
                 return false;
             }
         });
-        searchView.setOnSuggestionListener(new SearchView.OnSuggestionListener() {
-            @Override
-            public boolean onSuggestionSelect(int i) {
-                return false;
-            }
-
-            @Override
-            public boolean onSuggestionClick(int i) {
-                return false;
-            }
-        });
-
-
-
-
-
-
-        /*
-        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                //TODO: show Tutoractivity
-
-                Intent showDetail = new Intent(getApplicationContext(), DetailActivity.class);
-                showDetail.putExtra("de.lmu.sajko.ITEM.INDEX", i);
-                startActivity(showDetail);
-
-            }
-        });*/
     }
 
-    private void search() {
+    private void fetchUsers() {
+        databaseHelper = new SQLiteDBHelper(activity);
+        userArrayList.addAll(databaseHelper.getAllTutors());
+        searchAdapter.notifyDataSetChanged();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -167,6 +154,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             profileactivity.putExtra("EMAIL", email);
             startActivity(profileactivity);
         }
+        return false;
+    }
+
+    @Override
+    public boolean onSuggestionSelect(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean onSuggestionClick(int i) {
         return false;
     }
 }
