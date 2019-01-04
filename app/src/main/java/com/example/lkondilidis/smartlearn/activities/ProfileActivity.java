de8977ed91f61dc14ebe8448f7dea4530424e4ae
lkondilidis <lidia.kondilidis@yahoo.com>
2019-01-05 00:50:36 +0100

    bewertung in my profile also

--- app/src/main/java/com/example/lkondilidis/smartlearn/activities/ProfileActivity.java
+++ app/src/main/java/com/example/lkondilidis/smartlearn/activities/ProfileActivity.java
@@ -150,6 +150,9 @@ public class ProfileActivity extends AppCompatActivity {
         if (!STRING_EMPTY.equals(currentuser.getPlan())) {
             textViewPlan.setText(currentuser.getPlan());
         }
+        if (!STRING_EMPTY.equals(currentuser.getRatings())) {
+            textViewPlan.setText(String.valueOf(currentuser.getRatings()));
+        }
         else {
             Toast.makeText(ProfileActivity.this, "Please fill you personal data",
                     Toast.LENGTH_LONG).show();
@@ -300,13 +303,13 @@ public class ProfileActivity extends AppCompatActivity {
                 userDetails.setStudies(editTextStudies.getText().toString());
                 userDetails.setSubject(editTextSubject.getText().toString());
                 userDetails.setPlan(planfinal);
-                //userDetails.setRatings(Integer.parseInt(editTextRatings.getText().toString()));
+
 
                 dataBaseHelper.updateUser(userDetails);
                 textViewStudies.setText(userDetails.getStudies());
                 textViewSubject.setText(userDetails.getSubject());
                 textViewPlan.setText(userDetails.getPlan());
-                //textViewRatings.setText(userDetails.getRatings());
+
 
                 submitClicked();
                 Toast.makeText(ProfileActivity.this, "EDITED!",
