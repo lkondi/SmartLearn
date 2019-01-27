f8302b147ef3c75fa2993cad262978f6bfc2f269
lkondilidis <lidia.kondilidis@yahoo.com>
2019-01-27 14:53:02 +0100

    bullshit

--- app/src/main/java/com/example/lkondilidis/smartlearn/activities/Appointment_List_Activity.java
+++ app/src/main/java/com/example/lkondilidis/smartlearn/activities/Appointment_List_Activity.java
@@ -15,26 +15,30 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ListView;
 import com.example.lkondilidis.smartlearn.R;
+import com.example.lkondilidis.smartlearn.adapters.AppointmentAdapter;
+import com.example.lkondilidis.smartlearn.adapters.RatingAdapter;
 import com.example.lkondilidis.smartlearn.helpers.DrawerNavigationListener;
 import com.example.lkondilidis.smartlearn.model.Appointment;
+import com.example.lkondilidis.smartlearn.model.Rating;
 import com.example.lkondilidis.smartlearn.model.User;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Appointment_List_Activity extends AppCompatActivity {
     private final AppCompatActivity activity = Appointment_List_Activity.this;
 
-    private static String STRING_EMPTY = "";
-
     private DrawerLayout drawerLayout;
 
     private String intentAction;
-    private User currentuser, selecteduser;
-    private ImageView imageconfirmed, imagepending;
-
-
-    private TextView appuser, appsubject, appdate, usernameHeader;
+    User currentuser, selecteduser;
+    AppointmentAdapter appAdapter;
+    ListView listViewappoint;
+    ImageView imageconfirmed, imagepending;
+    ArrayList<Appointment> appointmentlist = new ArrayList<>();
+    TextView appuser, appsubject, appdate, usernameHeader;
 
     public static final String USER_DETAIL_KEY = "currentuser";
     public static final String SELECTED_USER_DETAIL_KEY = "selecteduser";
@@ -67,77 +71,12 @@ public class Appointment_List_Activity extends AppCompatActivity {
         appsubject = (TextView) findViewById(R.id.app_subject);
         imageconfirmed = (ImageView) findViewById(R.id.image_accepted);
         imagepending = (ImageView) findViewById(R.id.image_pending);
+        listViewappoint = (ListView) findViewById(R.id.listViewAppointments);
 
-    }
+        appointmentlist = (ArrayList)currentuser.getAppointments();
+        appAdapter = new AppointmentAdapter(this, R.layout.apppointment_list_item, appointmentlist);
+        listViewappoint.setAdapter(appAdapter);
 
-    class ListAdapter extends ArrayAdapter {
-        private ArrayList<Appointment> appointmentList = new ArrayList();
-        private Context context;
-        Appointment appointment = new Appointment();
-
-        class ItemViewHolder {
-            TextView author_name, app_subject, app_date;
-            ImageView confirmed, pending;
-        }
-
-        public ListAdapter(Context context, int textViewResourceId) {
-            super(context, textViewResourceId);
-            this.context = context;
-        }
-
-        //@Override
-        public void add(Appointment object) {
-            appointmentList.add(object);
-            super.add(object);
-        }
-
-        @Override
-        public int getCount() {
-            return this.appointmentList.size();
-        }
-
-        @Override
-        public Appointment getItem(int index) {
-            return this.appointmentList.get(index);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View row = convertView;
-            Appointment_List_Activity.ListAdapter.ItemViewHolder viewHolder;
-            if (row == null) {
-                LayoutInflater inflater = (LayoutInflater) this.getContext().
-                        getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                row = inflater.inflate(R.layout.apppointment_list_item, parent, false);
-                viewHolder = new Appointment_List_Activity.ListAdapter.ItemViewHolder();
-                viewHolder.author_name = (TextView) row.findViewById(R.id.author_name);
-                viewHolder.app_subject = (TextView) row.findViewById(R.id.app_subject);
-                viewHolder.app_date = (TextView) row.findViewById(R.id.app_date);
-                viewHolder.confirmed = (ImageView) row.findViewById(R.id.image_accepted);
-                viewHolder.pending = (ImageView) row.findViewById(R.id.image_pending);
-                row.setTag(viewHolder);
-            } else {
-                viewHolder = (Appointment_List_Activity.ListAdapter.ItemViewHolder)row.getTag();
-            }
-
-            final Appointment appointment = getItem(position);
-            viewHolder.author_name.setText(appointment.getAppointmentAuthor().getName());
-            viewHolder.app_subject.setText(appointment.getDate());
-            viewHolder.app_date.setText(appointment.getSubject().getName());
-
-
-            if(appointment.getAccepted()) {
-                viewHolder.confirmed.setVisibility(View.VISIBLE);
-                viewHolder.confirmed.setVisibility(View.GONE);
-
-            }else  {
-                viewHolder.confirmed.setVisibility(View.GONE);
-                viewHolder.confirmed.setVisibility(View.VISIBLE);
-            }
-
-            return row;
-
-        }
     }
 
     @Override
---
+++ app/src/main/java/com/example/lkondilidis/smartlearn/adapters/AppointmentAdapter.java
@@ -0,0 +1,101 @@
+package com.example.lkondilidis.smartlearn.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import com.example.lkondilidis.smartlearn.R;
+import com.example.lkondilidis.smartlearn.model.Appointment;
+import com.example.lkondilidis.smartlearn.model.Rating;
+
+
+public class AppointmentAdapter extends ArrayAdapter {
+
+    private AppCompatActivity activity;
+    private List<Appointment> arrayList;
+
+    public AppointmentAdapter(AppCompatActivity context, int resource, List<Appointment> objects) {
+        super(context, resource, objects);
+        this.activity = context;
+        this.arrayList = objects;
+    }
+
+    @Override
+    public Appointment getItem(int position) {
+        return arrayList.get(position);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.apppointment_list_item, parent, false);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+            //holder.ratingBar.getTag(position);
+        }
+
+        holder.author_name.setText(getItem(position).getAppointmentAuthor().getName());
+        holder.app_subject.setText(getItem(position).getSubject().getName());
+        holder.app_date.setText(getItem(position).getDate());
+
+        if(getItem(position).getAccepted()) {
+            holder.confirmed.setVisibility(View.VISIBLE);
+            holder.pending.setVisibility(View.GONE);
+        } else {
+            holder.confirmed.setVisibility(View.GONE);
+            holder.pending.setVisibility(View.VISIBLE);
+        }
+
+        return convertView;
+    }
+
+    public void setDataSet(Set<Appointment> userAppointments) {
+        for(Appointment a: userAppointments){
+            if(appointmentsDoNotContain(a)){
+                arrayList.add(a);
+            }
+        }
+        this.notifyDataSetChanged();
+    }
+
+    private boolean appointmentsDoNotContain(Appointment a) {
+        for(Appointment appointment: this.arrayList){
+            if(a.getId() == appointment.getId()){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static class ViewHolder {
+        private ImageView confirmed, pending;
+        private TextView author_name, app_subject, app_date;
+
+        public ViewHolder(View view) {
+            confirmed = (ImageView) view.findViewById(R.id.image_accepted);
+            pending = (ImageView) view.findViewById(R.id.image_pending);
+            author_name = (TextView) view.findViewById(R.id.author_name);
+            app_date = (TextView) view.findViewById(R.id.app_subject);
+            app_subject = (TextView) view.findViewById(R.id.app_date);
+        }
+    }
+}
